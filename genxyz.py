b6d7d733d5e258c8dc47c2bcd4bad4c453b4f552
iamusen <musen.li@student.uts.edu.au>
2022-07-09 10:54:40 +0800

    Upload data

---
+++ genxyz.py
@@ -0,0 +1,11 @@
+import numpy as np
+from ase.io import read, write 
+vaspobj = read('vasprun.xml', index=":")
+confnam = "liquid"
+for ind, ib in enumerate(vaspobj):
+    xx,yy,zz,yz,xz,xy=-vaspobj[ind].calc.results['stress']*vaspobj[ind].get_volume()
+    vaspobj[ind].info['virial']= np.array([(xx, xy, xz), (xy, yy, yz), (xz, yz, zz)])
+    del vaspobj[ind].calc.results['stress']
+    vaspobj[ind].pbc=True
+    vaspobj[ind].info['config_type']="liquid"
+    write(".xyz",vaspobj[ind],append=True)
